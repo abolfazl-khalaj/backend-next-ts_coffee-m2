bbb7e679e6f4d1245fad0ae2709993e4657a227a
abolfazl-khalaj <abolfazlkhalaj.dev@gmail.com>
2025-02-09 17:58:46 +0330

    optimization backend

--- app/api/auth/user/route.ts
+++ app/api/auth/user/route.ts
@@ -6,6 +6,19 @@ import { NextRequest, NextResponse } from "next/server";
 
 
 
+export async function GET(req:NextRequest):Promise<NextResponse> {
+    
+    try {
+
+        connectedDB()
+
+        const users = await UserModel.find()
+        return NextResponse.json({users})
+    } catch (error) {
+        return NextResponse.json({error})
+    }
+}
+
 export async function POST(req:NextRequest):Promise<NextResponse> {
     
     try {
--- app/api/comment/[id]/route.ts
+++ app/api/comment/[id]/route.ts
@@ -3,21 +3,16 @@ import CommentModel from "@/model/Comment";
 import { NextRequest, NextResponse } from "next/server";
 
 
-export async function PUT(req:NextRequest, { params }: { params: { id: string } }):Promise<NextResponse> {
-    console.log(params);
-    
+export async function PUT(req:NextRequest, { params }: { params: { id: string } }):Promise<NextResponse> {    
     try{
-
         await connectedDB()
+        const comment = await CommentModel.findById(params.id)
+        await CommentModel.findByIdAndUpdate(params.id , {isAccept : !comment.isAccept})
 
-        await CommentModel.findByIdAndUpdate(params.id , {isAccept : true})
-
-        return NextResponse.json({message : 'accept comment successfully ...'})
-
+        return NextResponse.json({message : 'toggle status accept comment successfully ...'})
     }catch (error){
         console.log(error);
         
         return NextResponse.json({message : error},{status:500})
     }
-
 }
